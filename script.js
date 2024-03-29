ea63dc6d73c4af93fd2605d37a22205fe535e026
Merveille94 <emanueleguehi@gmail.com>
2024-03-29 06:32:24 +0000

    Update

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-function toggleCard(cardId) {
-    var cards = document.querySelectorAll('.card-container');
-    cards.forEach(function(card) {
-      if (card.id === cardId) {
-        if (card.style.display === 'block') {
-          card.style.display = 'none';
-        } else {
-          card.style.display = 'block';
-        }
+function toggleCard(cardId, element) {
+  var cards = document.querySelectorAll(".card-container");
+  cards.forEach(function (card) {
+    if (card.id === cardId) {
+      if (card.style.display === "block") {
+        card.style.display = "none";
       } else {
-        card.style.display = 'none';
+        card.style.display = "block";
       }
-    });
-  }
+    } else {
+      card.style.display = "none";
+    }
+  });
+  // Remove 'active' class from all elements
+  var allGalleryImgs = document.querySelectorAll(".gallery-img");
+  allGalleryImgs.forEach(function (el) {
+    el.classList.remove("active1");
+  });
+
+  element.classList.toggle("active1");
+}
